395873f5236f4ef0d368df50e840a114394db8f7
Giovanne Tarcitano <44866256+Gitarcitano@users.noreply.github.com>
2020-06-10 22:23:23 -0300

    layout

--- src/server.js
+++ src/server.js
@@ -5,16 +5,27 @@ const server = express()
 // configurar pasta publica
 server.use(express.static("public"))
 
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // configurar caminhos da minha aplicação
 // pagina inicial
 // req: Requisição
 // res: Resposta
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Um título" })
 })
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 // Ligar o servidor
