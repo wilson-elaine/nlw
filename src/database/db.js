dc226968b3fdbe1d3133c1878b564dbb3f4f73da
Giovanne Tarcitano <44866256+Gitarcitano@users.noreply.github.com>
2020-06-12 19:50:21 -0300

    aula 5

--- src/database/db.js
+++ src/database/db.js
@@ -12,7 +12,7 @@ db.serialize(() => {
     // 1 criar uma tabela
     db.run(`
         CREATE TABLE IF NOT EXISTS places (
-            id INTERGER PRIMARY KEY AUTOINCRENENT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             image TEXT,
             name TEXT,
             address TEXT,
@@ -36,7 +36,7 @@ db.serialize(() => {
         ) VALUES (?,?,?,?,?,?,?);
 
     `
-    db.run(query, {
+    const values = [
         "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80",
         "Colectoria",
         "Guilherme Gemballa, Jardim América",
@@ -44,12 +44,39 @@ db.serialize(() => {
         "Santa Catarina",
         "Rio do Sul",
         "Resíduos Eletrônicos, Lâmpadas"
+    ]
 
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
 
-    })
+        console.log("Cadastro com sucesso")
+        console.log(this)
+    }
+
+    db.run(query, values, afterInsertData)
+
 
     // 3 Consultar os dados da tabela
+    // db.all(`SELECT name FROM places`, function(err, rows) {
+    //     if (err) {
+    //         return console.log(err)
+
+    //     }
+
+    //     console.log("Aqui estão seus registros: ")
+    //     console.log(rows)
+    // })
 
     // 4 Deletar um dado da tabela
+    // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Registro deletado com sucesso")
+    // })
+
 
 })
